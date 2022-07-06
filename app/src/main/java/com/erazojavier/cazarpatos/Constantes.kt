6cba863ec21c36f9d5fff66eed2f31b05fa356f6
Javier Erazo <alex.erazo01@epn.edu.ec>
2022-07-06 16:37:36 -0500

    update/SharedPreferences - archivos externos - encr

--- app/src/main/java/com/erazojavier/cazarpatos/Constantes.kt
+++ app/src/main/java/com/erazojavier/cazarpatos/Constantes.kt
@@ -1,3 +1,8 @@
 package com.erazojavier.cazarpatos
 
 const val EXTRA_LOGIN = "EXTRA_LOGIN"
+const val MIN_PASSWORD_LENGTH = 8
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
+const val ARCHIVO_DATOS_PATOS = "mySharedInformation.dat"
+const val CLAVES_PATOS_ENCRIPT = "claves_patos"
--- app/src/main/java/com/erazojavier/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/erazojavier/cazarpatos/LoginActivity.kt
@@ -5,9 +5,16 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+    // //=================Manejo de Archivos=================
+    lateinit var manejadorArchivo: FileHandler
+    lateinit var manejadorArchivoExterno: FileHandler
+    lateinit var checkBoxRecordarme: CheckBox
+    //========================================================
+
     lateinit var editTextEmail: EditText
     lateinit var editTextPassword:EditText
     lateinit var buttonLogin: Button
@@ -21,6 +28,16 @@ class LoginActivity : AppCompatActivity() {
         editTextPassword = findViewById(R.id.editTextPassword)
         buttonLogin = findViewById(R.id.buttonLogin)
         buttonNewUser = findViewById(R.id.buttonNewUser)
+        // //=================Manejo de Archivos=================
+        manejadorArchivo = EncryptedSharedPreferencesManager(this)
+        checkBoxRecordarme = findViewById(R.id.checkBoxRecordarme)
+        LeerDatosDePreferencias()
+
+        //Grabar en un archivo los datos
+        manejadorArchivoExterno = FileExternalManager(this)
+        //========================================================
+
+
         //Eventos clic
         buttonLogin.setOnClickListener {
             val email = editTextEmail.text.toString()
@@ -28,6 +45,10 @@ class LoginActivity : AppCompatActivity() {
             //Validaciones de datos requeridos y formatos
             if(!ValidarDatosRequeridos())
                 return@setOnClickListener
+
+            //Guardar datos en preferencias.
+            GuardarDatosEnPreferencias()
+
             //Si pasa validación de datos requeridos, ir a pantalla principal
             val intencion = Intent(this, MainActivity::class.java)
             intencion.putExtra(EXTRA_LOGIN, email)
@@ -39,6 +60,7 @@ class LoginActivity : AppCompatActivity() {
         mediaPlayer=MediaPlayer.create(this, R.raw.title_screen)
         mediaPlayer.start()
     }
+
 
     private fun ValidarDatosRequeridos():Boolean{
         val email = editTextEmail.text.toString()
@@ -64,5 +86,31 @@ class LoginActivity : AppCompatActivity() {
         mediaPlayer.release()
         super.onDestroy()
     }
+    // //=================Manejo de Archivos=================
+    private fun LeerDatosDePreferencias(){
+        val listadoLeido = manejadorArchivo.ReadInformation()
+        if(listadoLeido.first != null){
+            checkBoxRecordarme.isChecked = true
+        }
+        editTextEmail.setText ( listadoLeido.first )
+        editTextPassword.setText ( listadoLeido.second )
+    }
+
+    private fun GuardarDatosEnPreferencias() {
+        val email = editTextEmail.text.toString()
+        val clave = editTextPassword.text.toString()
+        val listadoAGrabar: Pair<String, String>
+        if(checkBoxRecordarme.isChecked){
+            listadoAGrabar = email to clave
+        }
+        else{
+            listadoAGrabar ="" to ""
+        }
+        manejadorArchivo.SaveInformation(listadoAGrabar)
+        manejadorArchivoExterno.SaveInformation((listadoAGrabar))
+        println(manejadorArchivo.ReadInformation())
+    }
+    //========================================================
+
 }
 
